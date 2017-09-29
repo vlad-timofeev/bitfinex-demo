c5f792a2e1b135ec0ef240436f70b031066942fc
Vlad Timofeev <vtimofeev.m@gmail.com>
2017-09-29 13:14:11 +0700

    improve Ticker styling

--- src/components/Ticker.js
+++ src/components/Ticker.js
@@ -3,11 +3,13 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { getTicker } from 'src/redux/selectors';
-import { TICKER_PROPS } from 'src/redux/model';
+import { TICKER, TICKER_PROPS } from 'src/redux/model';
 
 
 const IMAGE_SCR = 'https://www.bitfinex.com/assets/' +
   'BTC-alt-1ca8728fcf2bc179dfe11f9a0126bc303bee888bff8132c5ff96a4873cf9f0fb.svg';
+
+const NA = 'N/A';
 
 function mapStateToProps(state) {
   return {
@@ -22,11 +24,23 @@ export default connect(mapStateToProps)(class extends React.PureComponent {
 
   render() {
     const { ticker } = this.props;
+    const lastPrice = (ticker) ? ticker[TICKER.LAST_PRICE] : NA;
+    const volume = (ticker) ? ticker[TICKER.VOLUME].toFixed(1) : NA;
+    const high = (ticker) ? ticker[TICKER.HIGH] : NA;
+    const low = (ticker) ? ticker[TICKER.LOW] : NA;
+
     return (
-      <div>
-        <img src={IMAGE_SCR} className="ticker-icon" alt="ticker" />
-        Ticker:
-        <div>{ticker ? JSON.stringify(ticker) : null}</div>
+      <div className="ticker">
+        <div>
+          <img src={IMAGE_SCR} className="ticker__icon" alt="ticker" />
+          <div><b>BTC/USD</b></div>
+        </div>
+        <div className="ticker__data">
+          <div>{`Last price: ${lastPrice}`}</div>
+          <div>{`Volume: ${volume}`}</div>
+          <div>{`High: ${high}`}</div>
+          <div>{`Low: ${low}`}</div>
+        </div>
       </div>
     );
   }
